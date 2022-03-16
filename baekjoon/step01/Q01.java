1e6690f05d5d26a4faa2d4362093aa47139a0c68
Heisely <ypd00062@gmail.com>
2022-03-16 14:52:19 +0900

    오래된 파일 삭제

--- baekjoon/step01/Q01.java
+++
@@ -1,8 +0,0 @@
-package baekjoon.step01;
-
-// 2557. Hello World!
-public class Q01 {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
-}
--- tsoj/ch07/Ex09.java
+++
@@ -1,9 +0,0 @@
-package tsoj.ch07;
-
-// PackageTest.
-public class Ex09 {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		// 자바의 정석 1권 p.337 ~ p.339 참고
-	}
-}
